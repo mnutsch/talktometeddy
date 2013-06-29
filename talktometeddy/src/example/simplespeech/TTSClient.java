0c4661b0d7b01493084c98f8fd99468d9d580e42
liang3404814 <liang3404814@gmail.com>
2013-06-29 13:09:44 -0700

    Isolated TTSClient

---
+++ talktometeddy/src/example/simplespeech/TTSClient.java
@@ -0,0 +1,11 @@
+package example.simplespeech;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liang3404814
+ * Date: 6/29/13
+ * Time: 12:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TTSClient {
+}
