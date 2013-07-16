c83e0e5a5edbc47ca6811bebcc08834d01922bf9
Matt Nutsch <mattnutsch@gmail.com>
2013-07-16 12:29:06 -0700

    added Google Analytics event tracking
    removed duplicate ReadyForSpeech() function call
    corrected a typo "Ops" to "Oops"
    added google analytics events to each user statement
    added some comments to help keep the purpose of the code clear

---
+++ src/example/simplespeech/MainActivity.java
@@ -0,0 +1,53 @@
+package example.simplespeech;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import com.example.talktometeddy.R;
+
+/**
+ * A simple activity launcher.
+ **/
+public class MainActivity extends ListActivity
+{
+    /**
+     * Display a list of sample activities.
+     **/
+    @Override
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+
+        // Display the list of sample activities in a standard Android layout.
+
+        activities = new Item[] {
+                new Item(new Intent(this, StartScreenActivity.class),
+                        getString(R.string.app_name_launch_screen))
+
+        };
+
+        startActivity(activities[0].intent);
+    }
+
+    /**
+     * Represents an activity for the list view.
+     **/
+    private class Item {
+        final Intent intent;
+        final String title;
+        Item(Intent intent, String title) {
+            this.intent = intent; this.title = title;
+        }
+        @Override public String toString() {
+            return title;
+        }
+    }
+
+    private Item[] activities;
+
+    @Override
+    public void onListItemClick(ListView parent, View view, int position, long id) {
+        startActivity(activities[position].intent);
+    }
+}
