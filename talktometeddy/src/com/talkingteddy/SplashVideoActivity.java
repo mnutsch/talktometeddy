ad1a790afdbc300380959025d8be68ecd6036d89
liang3404814 <liang3404814@gmail.com>
2013-07-28 15:32:58 -0700

    added video player

---
+++ talktometeddy/src/com/talkingteddy/SplashVideoActivity.java
@@ -0,0 +1,17 @@
+package com.talkingteddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liang3404814
+ * Date: 7/28/13
+ * Time: 2:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SplashVideoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
